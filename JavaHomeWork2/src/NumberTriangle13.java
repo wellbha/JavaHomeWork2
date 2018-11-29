36a49f95c0b265a4e1f3269b696563b274a2dd95
wellbha <wellbha@gmail.com>
2018-11-29 22:33:31 +0000

    NumberTriangle13 now updated with desired output

--- JavaHomeWork2/src/NumberTriangle13.java
+++ JavaHomeWork2/src/NumberTriangle13.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
-public class NumberTriangle13 {
-
-    public static void main(String[] args) {
+public class NumberTriangle13
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any number to create number triangle");
         int a = sc.nextInt();
         System.out.println("The number triangle");
 
-        for (int i = 0; i <= a; i++) {
-
-            for (int j = a-1; j >= i; j--) {
-               // System.out.print(j);
-                System.out.print(" ");
+        for (int i = 0; i <= a; i++)
+        {
+            for (int j = a-1; j >= i; j--)
+            {
+                System.out.print("  ");
             }
-            for ( int k = 1; k <= i; k++ ){
-
+            for ( int k = 1; k <= i; k++ )
+            {
                 System.out.print(k + " ");
             }
-                System.out.print(" ");
-
-            System.out.println();
+            System.out.println(" ");
         }
 
     }
