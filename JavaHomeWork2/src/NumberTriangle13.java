932d50b369da1004f40bad301d4f9e044b1b29ae
wellbha <wellbha@gmail.com>
2018-11-29 22:18:30 +0000

    number triangle again

--- JavaHomeWork2/src/NumberTriangle13.java
+++ JavaHomeWork2/src/NumberTriangle13.java
@@ -7,11 +7,19 @@ public class NumberTriangle13 {
         System.out.println("Enter any number to create number triangle");
         int a = sc.nextInt();
         System.out.println("The number triangle");
-        for (int i = 0; i < a; i++) {
-            for (int j = 1; j <= i + 1; j++) {
-                System.out.print(j);
+
+        for (int i = 0; i <= a; i++) {
+
+            for (int j = a-1; j >= i; j--) {
+               // System.out.print(j);
                 System.out.print(" ");
             }
+            for ( int k = 1; k <= i; k++ ){
+
+                System.out.print(k + " ");
+            }
+                System.out.print(" ");
+
             System.out.println();
         }
 
